6b7c098cfb3dac66460cc2789824d4da0a8c7e0b
rafaelvieiracosta <rvc.vieira@outlook.com>
2022-06-15 14:20:07 -0300

    feat: script add and remove from cart

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const vm = new Vue({
   el: '#app',
   data:{
     produtos: [],
-    produto: ''
+    produto: '',
+    carrinho: [],
+    mensagemAlerta: "Item adicionado",
+    alertaAtivo: false
   },
   filters: {
     numeroPreco(valor){
       return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})
+    }
+  },
+  computed:{
+    carrinhoTotal(){
+      let total = 0;
+      if(this.carrinho.length){
+        this.carrinho.forEach(item => {
+          total += item.preco
+        })
+      }
+      return total;
     }
   },
   methods: {
@@ -23,9 +37,45 @@ const vm = new Vue({
       .then(r => {
         this.produto = r
       })
+    },
+    abrirModal(id){
+      this.fetchProduto(id)
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    fecharModal({target, currentTarget}){
+      if(target === currentTarget) 
+        this.produto = false
+    },
+    adicionarItem(){
+      this.produto.estoque--;
+      const {id, nome, preco} = this.produto
+      this.carrinho.push({id, nome, preco})
+      this.alerta(`${nome} foi adicionado ao carrinho`)
+    },
+    removerItem(index){
+      this.carrinho.splice(index, 1)
+    },
+    checarLocalStorage(){
+      if(window.localStorage.carrinho) this.carrinho = JSON.parse(window.localStorage.carrinho)
+    },
+    alerta(mensagem){
+      this.mensagemAlerta = mensagem
+      this.alertaAtivo = true
+      setTimeout(() => {
+        this.alertaAtivo = false
+      }, 1500)
+    }
+  },
+  watch:{
+    carrinho(){
+      window.localStorage.carrinho = JSON.stringify(this.carrinho)
     }
   },
   created(){
     this.fetchProdutos()
+    this.checarLocalStorage()
   }
 })
