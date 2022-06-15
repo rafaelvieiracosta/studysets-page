cc6d503771a3b09d6040bbea2f9107c7e6b9ea88
rafaelvieiracosta <rvc.vieira@outlook.com>
2022-06-15 14:27:00 -0300

    feat: router

--- app.js
+++ app.js
@@ -67,15 +67,27 @@ const vm = new Vue({
       setTimeout(() => {
         this.alertaAtivo = false
       }, 1500)
+    },
+    router(){
+      const hash = document.location.hash;
+      if(hash){
+        this.fetchProduto(hash.replace("#", ""))
+      }
     }
   },
   watch:{
     carrinho(){
       window.localStorage.carrinho = JSON.stringify(this.carrinho)
+    },
+    produto(){
+      document.title = this.produto.nome || "StudySets"
+      const hash = this.produto.id || ""
+      history.pushState(null, null, `#${hash}`)
     }
   },
   created(){
     this.fetchProdutos()
+    this.router()
     this.checarLocalStorage()
   }
 })
